9536b1d1139e49abb6eb130855bc25975cd9da65
cuixiuyin <1099442418@qq.com>
2018-12-19 21:40:28 +0800

    framework initial commit

---
+++ client/src/main/java/org/dubbo/client/Client.java
@@ -0,0 +1,13 @@
+package org.dubbo.client;
+
+/**
+ * @Description:
+ * @author: cuixiuyin
+ * @date: 2018/12/19 21:32
+ */
+public class Client {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ server/src/main/java/org.dubbo.server/Server.java
@@ -0,0 +1,14 @@
+package org.dubbo.server;
+
+/**
+ * @Description:
+ * @author: cuixiuyin
+ * @date: 2018/12/19 21:30
+ */
+
+public class Server {
+
+    public static void main(String[] args) {
+
+    }
+}
