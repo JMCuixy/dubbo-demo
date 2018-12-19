e9a720e0b97512c94b6ac5f47156d6dc060d98aa
cuixiuyin <1099442418@qq.com>
2018-12-19 22:05:51 +0800

    framework initial commit

--- client/src/main/java/org/dubbo/client/Client.java
+++ client/src/main/java/org/dubbo/client/Client.java
@@ -1,13 +1,17 @@
 package org.dubbo.client;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Description:
  * @author: cuixiuyin
  * @date: 2018/12/19 21:32
  */
+@SpringBootApplication
 public class Client {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Client.class, args);
     }
 }
--- server/src/main/java/org.dubbo.server/Server.java
+++ server/src/main/java/org.dubbo.server/Server.java
@@ -1,14 +1,17 @@
 package org.dubbo.server;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @Description:
  * @author: cuixiuyin
  * @date: 2018/12/19 21:30
  */
-
+@SpringBootApplication
 public class Server {
 
     public static void main(String[] args) {
-
+        SpringApplication.run(Server.class, args);
     }
 }
